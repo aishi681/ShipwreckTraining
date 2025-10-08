ecbb0762592ddfb8137e308d4c28c63518a2608f
Vedaant Adavikolanu <avedaant@gmail.com>
2025-10-07 20:12:25 -0700

    YArm Changes - 1. Changed SHOULDER_MOTOR to SLIDE_MOTOR

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/YArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/YArm.java
@@ -7,9 +7,9 @@ import com.jumpypants.murphy.tasks.Task;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class YArm {
-    private final Motor SHOULDER_MOTOR;
+    private final Motor SLIDE_MOTOR;
 
-    private final PIDController SHOULDER_PID = new PIDController(0.015, 0, 0.003);
+    private final PIDController SLIDE_PID = new PIDController(0.015, 0, 0.003);
     private double targetPosition = 0.0;
 
     public static double SHOULDER_INTAKING_POSITION = 0.0;
@@ -17,23 +17,23 @@ public class YArm {
 
     public YArm(HardwareMap hardwareMap) {
 
-        SHOULDER_MOTOR = new Motor(hardwareMap, "shoulderMotor");
+        SLIDE_MOTOR = new Motor(hardwareMap, "shoulderMotor");
 
-        SHOULDER_MOTOR.setRunMode(Motor.RunMode.RawPower);
+        SLIDE_MOTOR.setRunMode(Motor.RunMode.RawPower);
 
-        SHOULDER_MOTOR.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        SLIDE_MOTOR.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
 
-        SHOULDER_MOTOR.resetEncoder();
+        SLIDE_MOTOR.resetEncoder();
     }
 
     public void updatePID() {
-        double currentPosition = SHOULDER_MOTOR.getCurrentPosition();
+        double currentPosition = SLIDE_MOTOR.getCurrentPosition();
 
-        double pidOutput = SHOULDER_PID.calculate(currentPosition, targetPosition);
+        double pidOutput = SLIDE_PID.calculate(currentPosition, targetPosition);
 
         pidOutput = Math.max(-1.0, Math.min(1.0, pidOutput));
 
-        SHOULDER_MOTOR.set(pidOutput);
+        SLIDE_MOTOR.set(pidOutput);
     }
 
 
@@ -47,7 +47,7 @@ public class YArm {
         }
 
         public void setShoulderTarget(double targetTicks) {
-            SHOULDER_PID.setSetPoint(targetTicks);
+            SLIDE_PID.setSetPoint(targetTicks);
         }
 
         @Override
@@ -57,7 +57,7 @@ public class YArm {
 
         @Override
         protected boolean run(RobotContext robotContext) {
-            return Math.abs(SHOULDER_MOTOR.getCurrentPosition() - targetPosition) > 5;
+            return Math.abs(SLIDE_MOTOR.getCurrentPosition() - targetPosition) > 5;
         }
     }
 }
