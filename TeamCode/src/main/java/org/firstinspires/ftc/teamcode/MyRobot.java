3d1c53f0da79e02e146713161df1fe73ebd6b5e3
Abhi2009CU <146779945+Abhi2009CU@users.noreply.github.com>
2025-10-07 19:41:52 -0700

    Create MyRobot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyRobot.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.jumpypants.murphy.RobotContext;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.subSystems.Bucket;
+import org.firstinspires.ftc.teamcode.subSystems.Wrist;
+import org.firstinspires.ftc.teamcode.subSystems.Xarm;
+import org.firstinspires.ftc.teamcode.subSystems.Yarm;
+
+/**
+ * MyRobot class that extends RobotContext to include robot-specific subsystems.
+ */
+public class MyRobot extends RobotContext {
+    public final MecanumDrive drive;
+    public final Xarm xarm;
+    public final Yarm yarm;
+    public final Wrist wrist;
+    public final Bucket bucket;
+
+    //not sure about the following
+    public Gamepad gamepad1, gamepad2;
+
+    /**
+     * Creates a new RobotContext with the specified telemetry and gamepad references.
+     * All parameters are required and cannot be null.
+     *
+     * @param telemetry the telemetry instance for driver station communication
+     * @param gamepad1  the primary gamepad controller
+     * @param gamepad2  the secondary gamepad controller
+     */
+    public MyRobot(Telemetry telemetry, Gamepad gamepad1, Gamepad gamepad2, MecanumDrive drive, Xarm xarm, Yarm yarm, Wrist wrist, Bucket bucket, Yarm yarm1, Wrist wrist1, Bucket bucket1) {
+        super(telemetry, gamepad1, gamepad2);
+        this.drive = drive;
+        this.xarm = xarm;
+        this.yarm = yarm1;
+        this.wrist = wrist1;
+        this.bucket = bucket1;
+
+        //not sure about the following
+        this.gamepad1 = gamepad1;
+        this.gamepad2 = gamepad2;
+
+    }
+}
